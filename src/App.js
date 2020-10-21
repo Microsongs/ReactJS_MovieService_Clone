4f491281c5c44d022023e789c6f22d1c173bed8a
현석 송 <jhn06046@hotmail.com>
2020-10-21 11:07:49 +0900

    JSX/PROPS ~#2.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+function Food({name, picture}){
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name} />
+  </div>
+}
+
 function App() {
+  // map은 각각의 아이템별로 function을 호출한다.
+  // console.log에서는 food name = {} />을 하고 있다
+  // map은 리스트 안에 들어갈 때 유일성을 잃어버림 -> id를 삽입
   return (
     <div className="App">
-      <h1>GHello</h1>
-      Hello!!!!
+      {foodILike.map(dish =>(
+        <Food key = {dish.id} name={dish.name} picture = {dish.image} />
+      ))}
     </div>
   );
 }
