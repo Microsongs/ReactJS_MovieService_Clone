cedae3830419eb98562f44db80e6cad61178696c
현석 송 <jhn06046@hotmail.com>
2020-10-23 09:48:17 +0900

    3. JSX and Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
-function Food({name, picture}){
+const foodILike = [
+  {
+    id : 1,
+    name : "Kimchi",
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR1HuLwzPb9tpfKtGCWVlMW-vipU_K3D7ZLoQ&usqp=CAU",
+    rating : 5
+  },
+  {
+    id : 2,
+    name : "ramen",
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSZy8Xa_-V96PpI8hjj1l7bUpg5K0EE2RIQKQ&usqp=CAU",
+    rating : 4.8
+  },
+  {
+    id : 3,
+    name : "tonkatsu",
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQdUCrIgLf1JU6SADwqgv2GYyy0xp0_dm7fsg&usqp=CAU",
+    rating : 4.5
+  },
+  {
+    id : 4,
+    name : "kimbap",
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRnhjF8IiOTERtznA6rr9JmSa-JzELZlNM2NA&usqp=CAU",
+    rating : 4.2
+  }
+  
+]
+
+function Food({name, picture, rating}){
   return <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name} />
   </div>
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+};
 
 function App() {
   // map은 각각의 아이템별로 function을 호출한다.
@@ -14,7 +50,11 @@ function App() {
   return (
     <div className="App">
       {foodILike.map(dish =>(
-        <Food key = {dish.id} name={dish.name} picture = {dish.image} />
+        <Food 
+         key = {dish.id}
+         name={dish.name} 
+         picture = {dish.image} 
+         rating={dish.rating} />
       ))}
     </div>
   );
